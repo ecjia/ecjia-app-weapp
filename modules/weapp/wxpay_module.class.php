8f15b043e6641c4b2aef0a3bd47fce7347066ed5
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-07-20 16:44:55 +0800

    更新门店微信支付方式

--- modules/weapp/wxpay_module.class.php
+++ modules/weapp/wxpay_module.class.php
@@ -88,15 +88,6 @@ class wxpay_module extends api_front implements api_interface {
 		    return $handler;
 		}
 		
-// 		/* 插入支付流水记录*/
-// 		RC_Api::api('payment', 'save_payment_record', [
-//     		'order_sn' 		 => $order['order_sn'],
-//     		'total_fee'      => $order['order_amount'],
-//     		'pay_code'       => $handler->getCode(),
-//     		'pay_name'		 => $handler->getName(),
-//     		'trade_type'	 => 'buy',
-// 		]);
-		
 		$handler->set_orderinfo($order);
 		$handler->setPaymentRecord(new Ecjia\App\Payment\Repositories\PaymentRecordRepository());
 	
