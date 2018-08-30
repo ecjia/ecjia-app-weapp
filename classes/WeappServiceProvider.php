fd1d467cc541cb783df5e394d2a181c7c76347d6
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-30 23:36:01 +0800

    指定ServiceProvider的路径

--- classes/WeappServiceProvider.php
+++ classes/WeappServiceProvider.php
@@ -9,7 +9,7 @@ class WeappServiceProvider extends  AppParentServiceProvider
     
     public function boot()
     {
-        $this->package('ecjia/app-weapp');
+        $this->package('ecjia/app-weapp', null, dirname(__DIR__));
     }
     
     public function register()
